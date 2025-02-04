2c31cb39af3d5e68c85a47c60b9f9d83a4fd75a4
RajveerMadrid <b2028rmsmadrid@pshs.edu.ph>
2025-02-04 22:12:50 +0800

    commit5

--- public/js/ClickerGame.js
+++ public/js/ClickerGame.js
@@ -1,17 +1,72 @@
 let button = document.querySelector(".buttonCost");
 let parsedButton = parseFloat(button.innerHTML);
 
-let clickerCost = document.querySelector(".clickerCost");
-let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let miniAutoCost = document.querySelector(".miniAutoCost");
+let parsedMiniAutoCost = parseFloat(miniAutoCost.innerHTML);
+let miniAutoLevel = document.querySelector(".miniAutoLevel");
+let miniAutoIncrease = document.querySelector(".miniAutoIncrease");
+let parsedMiniAutoIncrease = parseFloat(miniAutoIncrease.innerHTML);
+
+let smallAutoCost = document.querySelector(".smallAutoCost");
+let parsedSmallAutoCost = parseFloat(smallAutoCost.innerHTML);
+let smallAutoLevel = document.querySelector(".smallAutoLevel");
+let smallAutoIncrease = document.querySelector(".smallAutoIncrease");
+let parsedSmallAutoIncrease = parseFloat(smallAutoIncrease.innerHTML);
+
+let mediumAutoCost = document.querySelector(".mediumAutoCost");
+let parsedMediumAutoCost = parseFloat(mediumAutoCost.innerHTML);
+let mediumAutoLevel = document.querySelector(".mediumAutoLevel");
+let mediumAutoIncrease = document.querySelector(".mediumAutoIncrease");
+let parsedMediumAutoIncrease = parseFloat(mediumAutoIncrease.innerHTML);
+
+creditsPerClick = 1;
+let multiplier = 1.15
+let creditsPerSecond = 0;
 
 function incrementButton() {
-    parsedButton += 1;
-    button.innerHTML = parsedButton;
+    button.innerHTML = Math.round(parsedButton += creditsPerClick);
 }
 
-function buyClicker() {
-    if(parsedButton >= parsedClickerCost) {
-        parsedButton -= parsedClickerCost;
-        button.innerHTML = parsedButton;
+function buyMiniAuto() {
+    if(parsedButton >= parsedMiniAutoCost) {
+        button.innerHTML = Math.round(parsedButton -= parsedMiniAutoCost);
+
+        miniAutoLevel.innerHTML++;
+
+        creditsPerSecond += parsedMiniAutoIncrease
+
+        parsedMiniAutoCost *= multiplier;
+        miniAutoCost.innerHTML = Math.round(parsedMiniAutoCost)
     }
 }
+
+function buySmallAuto() {
+    if(parsedButton >= parsedSmallAutoCost) {
+        button.innerHTML = Math.round(parsedButton -= parsedSmallAutoCost);
+
+        smallAutoLevel.innerHTML++;
+
+        creditsPerSecond += parsedSmallAutoIncrease
+
+        parsedSmallAutoCost *= multiplier;
+        smallAutoCost.innerHTML = Math.round(parsedSmallAutoCost)
+    }
+}
+
+function buyMediumAuto() {
+    if(parsedButton >= parsedMediumAutoCost) {
+        button.innerHTML = Math.round(parsedButton -= parsedMediumAutoCost);
+
+        mediumAutoLevel.innerHTML++;
+        
+        creditsPerSecond += parsedMediumAutoIncrease
+
+        parsedMediumAutoCost *= multiplier;
+        mediumAutoCost.innerHTML = Math.round(parsedMediumAutoCost)
+    }
+}
+
+setInterval(() => {
+    parsedButton += creditsPerSecond/10;
+    button.innerHTML = Math.round(parsedButton);
+},100)
